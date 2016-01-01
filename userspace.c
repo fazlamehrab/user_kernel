6d644b5eac773ec90df303a7d4ebbdb67b3b2748
Sakib <fazla.m@samsugn.com>
2016-01-01 12:12:35 +0600

    Signal Handler added for ctrl+c and ctrl+z

--- userspace.c
+++ userspace.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <string.h>
@@ -6,6 +7,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 
 #define READ_IOCTL _IOR('G', 0, int)
 #define WRITE_IOCTL _IOW('G', 1, int)
@@ -13,7 +15,8 @@
 #define PAGE_SIZE 512
 
 char * address = NULL;
-int quit_thread = 0;
+int quit_thread = 0, handle, fd = -1;
+
 
 void* print_long()
 {	
@@ -33,14 +36,27 @@ void* print_long()
 
 }
 
+
+void intHandler(int data) 
+{
+	quit_thread = 1;
+    if(ioctl(fd, WRITE_IOCTL, "0") < 0)
+		perror("Exit Command");
+	close(handle);
+	//close(fd);
+	exit(1);
+}
+
 int main(){
 	char buf[20];
-	int fd = -1;
-	int option, handle, major=0, quit=0;
-	
+	int major=0, quit=0, already_allocated = 0;
+	char option[20];
 	pthread_t print_thread;	
 	FILE *myFile;
-
+	
+	signal(SIGINT, intHandler);
+	signal(SIGTSTP, intHandler);
+	
 	char *cdev = "/dev/my_char_device";
 	
 	if ((fd = open(cdev, O_RDWR)) < 0) {
@@ -50,51 +66,61 @@ int main(){
 
 	system("clear");
 
-while(!quit){
-	
-	printf("1. Allocate Memory at kernel space\n");
-	printf("2. Map Kernel space Memory\n");
-	//printf("3. Read Data\n");
-	//printf("0. Exit\n");
-	printf("Enter your choice :: ");
-	scanf("%d",&option);
+	while(!quit)
+	{
+		
+		printf("1. Allocate Memory at kernel space\n");
+		printf("2. Map Kernel space Memory\n");
+		//printf("3. Read Data\n");
+		//printf("0. Exit\n");
+		printf("Enter your choice :: ");
+		scanf("%s",option);
 
-	switch(option){
-		case 1:	
-			if(ioctl(fd, WRITE_IOCTL, "1") < 0)
-				perror("Allocate Command\n");
-			
-			handle = open("/sys/kernel/debug/memory_map", O_RDWR);
-			if(handle < 0) {
-				perror("File Open\n");
-				return -1;
-			}
-
-			break;
-		case 2:	
-			if(ioctl(fd, WRITE_IOCTL, "2") < 0)
-				perror("Map Command");
-			
-			address = mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, handle, 0);
-			if (address == MAP_FAILED) {
-				perror("Memory Map");
-				return -1;
-			}
-		//	break;
-		//case 3:
-			pthread_create(&print_thread, NULL, print_long, NULL); 
-			sleep(60);	
-			quit_thread = 1;
-			//break;
-		//case 0:
-			if(ioctl(fd, WRITE_IOCTL, "0") < 0)
-				perror("Exit Command");
-			close(handle);
-			close(fd);			
-			quit = 1;			
-			break;
+		switch(option[0])
+		{
+			case '1':	
+				if(!already_allocated)
+				{
+					if(ioctl(fd, WRITE_IOCTL, "1") < 0)
+					{
+						perror("Allocate Command\n");
+						return -1;
+					}	
+					already_allocated = 1;
+					handle = open("/sys/kernel/debug/memory_map", O_RDWR);
+					if(handle < 0) 
+					{
+						perror("File Open");
+						return -1;
+					}
+				}
+				break;
+			case '2':	
+				if(ioctl(fd, WRITE_IOCTL, "2") < 0)
+					perror("Map Command");
+				
+				address = mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, handle, 0);
+				if (address == MAP_FAILED) {
+					perror("Memory Map");
+					return -1;
+				}
+			//	break;
+			//case '3':
+				pthread_create(&print_thread, NULL, print_long, NULL); 
+				sleep(60);	
+				quit_thread = 1;
+				//break;
+			//case '0':
+				if(ioctl(fd, WRITE_IOCTL, "0") < 0)
+					perror("Exit Command");
+				close(handle);
+				close(fd);			
+				quit = 1;			
+				break;
+			default:
+				break;
+		}
 	}
-}
 
 	return 0;
 }
